efba31d2325e2217b07de99d0d0d8d48a4333c45
Michael Prattinger <mprattinger@outlook.com>
2020-04-08 23:01:48 +0200

    removed https

--- BackgardenBlazor/Startup.cs
+++ BackgardenBlazor/Startup.cs
@@ -55,7 +55,7 @@ namespace BackgardenBlazor
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
